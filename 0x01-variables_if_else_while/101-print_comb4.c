f036960c46ebfc629841855d58a1320500c0cf94
bravin-onwonga <onbravin@gmail.com>
2022-10-16 07:28:08 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = '0';
+	while (i <= '9')
+	{
+		j = i + 1;
+		while (j <= '9')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == '7' && j == '8' && k == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
