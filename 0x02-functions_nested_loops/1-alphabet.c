10eb3db1e86270a9f417c8ba3335fc1bce2259a0
bravin-onwonga <onbravin@gmail.com>
2022-10-18 06:53:22 -0700

    Alphabet in lowercase letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - prints the alphabet in lowercase
+ *followed by a new line
+ *Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
