2f72e732bd10be8355160e8180b32edac45cd38c
bravin-onwonga <onbravin@gmail.com>
2022-10-18 09:02:07 -0700

    Prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++
+	}
+}
