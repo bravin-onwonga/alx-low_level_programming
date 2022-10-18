6d09273c8079f9a1093f42a2006b12e2b6045fbf
bravin-onwonga <onbravin@gmail.com>
2022-10-18 11:35:47 -0700

    The last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,16 @@ int print_last_digit(int n)
 {
 	int l;
 
-	l = n % 10;
-	_putchar(l + '0') ;
-	return (l);
+	if (n > 0)
+	{
+		l = n % 10;
+		_putchar(l + '0');
+		return (l);
+	}
+	else if (n < 0)
+	{
+		l = -n % 10;
+		_putchar(l + '0');
+		return (l);
+	}
 }
