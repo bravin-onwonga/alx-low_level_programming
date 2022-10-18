00eb7e5af3685d2d1043f28fadc05bfdef8b3ee7
bravin-onwonga <onbravin@gmail.com>
2022-10-18 10:18:52 -0700

    Checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_islower - checks if c is in lowercase
+ *@c : is the value being checked
+ *Return: 1 if trueotherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
