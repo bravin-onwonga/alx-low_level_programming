2bd8efdf4ee88561cf00a09725c54a289f9c964a
bravin-onwonga <onbravin@gmail.com>
2022-10-19 06:48:38 -0700

    Prints the first 98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - first 50 Fibonacci numbers
+ *Return: 0, success
+ */
+int main(void)
+{
+	long int x, y;
+	long int temp;
+	int count = 0;/* count of numbers printed*/
+
+	x = 1;/* first value*/
+	y = 2;/*second value*/
+	printf("%lu, %lu", x, y);
+	while (count < 96)
+	{
+		if (count <= 95)
+		{
+			printf(", ");
+		}
+		temp = x + y;
+		printf("%lu", temp);
+		x = y;
+		y = temp;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
