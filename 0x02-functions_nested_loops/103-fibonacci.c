b494d4ab7905861a724d7bf41526ade3ed691458
bravin-onwonga <onbravin@gmail.com>
2022-10-19 06:39:53 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - sum of even numbers in Fibonacci series not exceeding 4, 000,00
+ *Return: 0, success
+ */
+int main(void)
+{
+	int sum;
+	long int temp;
+	long int x = 1, y = 2;
+
+	sum = 2;
+	while (temp <= 4000000)
+	{
+		temp = x + y;
+		if (temp % 2 == 0)
+			sum += temp;
+		x = y;
+		y = temp;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
