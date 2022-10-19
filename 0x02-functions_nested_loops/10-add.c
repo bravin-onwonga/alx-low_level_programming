55d30620fede673fafdb69f41a44f46ce21b3040
bravin-onwonga <onbravin@gmail.com>
2022-10-18 23:02:17 -0700

    Get the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - Calculates the sum of two integers
+ *@num1: first integer
+ *@num2: second integer
+ *Return: sum of num1 and num2
+ */
+
+int add(int num1, int num2)
+{
+	int result;/* holds the sum */
+
+	result = num1 + num2;
+	return (result);
+}
