9478837cd74682904f66bfc9065767fee91384e2
bravin-onwonga <onbravin@gmail.com>
2022-10-21 15:01:30 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_triangle - use the character '#' to draw a triangle
+ *@size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = i; j < size - 1; j++)
+				_putchar(' ');
+			for (j = 0; j < i + 1; j++))
+				_putchar('#');
+			_puthchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
