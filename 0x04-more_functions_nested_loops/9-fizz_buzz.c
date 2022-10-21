720199c3afea312483325858d5f61ebf3d09adde
bravin-onwonga <onbravin@gmail.com>
2022-10-21 13:28:01 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,11 +18,11 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
-		else if ((i % 5) == 0 && (i % 3) != 0)
+		else if ((i % 5 == 0) && (i % 15 != 0))
 		{
 			printf("Buzz ");
 		}
-		else if (i % 3 == 0)
+		else if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
