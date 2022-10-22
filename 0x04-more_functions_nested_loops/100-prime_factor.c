985a0348623ee088332a3f0cad2d6291322ec14f
bravin-onwonga <onbravin@gmail.com>
2022-10-22 02:01:08 -0700

    Largest prime factor of the number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long a, b;
+	
+	a = 612852475143;
+	for (b = 2; b < a; b++)
+	{
+		while (a % b == 0)
+		{
+			a = a / b;
+		}
+	}
+	printf("%lu", b);
+	putchar('\n');
+	return (0);
+}
