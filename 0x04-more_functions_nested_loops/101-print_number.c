e579252dc2b9e50714259d516929dd2078424bba
bravin-onwonga <onbravin@gmail.com>
2022-10-22 02:48:39 -0700

    Numbers have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *print_number - prints an integer
+ *@n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int k;
+	int a, b;
+
+	b = 10;
+	if (n < 10 && n >= 0)
+	{
+		_putchar(n + '0');
+	}
+	else if (n > -10 && n < 0)
+	{
+		n = n * -1
+		_putchar(n + '0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1;
+			_putchar('-');
+		}
+		k = n
+		while (n / 10 > 9)
+		{
+			b = b * 10;
+		}
+		while (b > 0)
+		{
+			a = k / b;
+			k = k % b;
+			_putchar(k + '0');
+			b = b / 10;
+		}
+	}
+}
