d05e65b74026851c6a3a11766308c7e7e7d1689e
bravin-onwonga <onbravin@gmail.com>
2022-10-25 00:02:55 -0700

    Update value it points

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - takes a pointer to an integer and updates the vaue it points
+ *@n: this is the pointer to the integer
+ *
+ *Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
