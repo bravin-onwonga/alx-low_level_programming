458d956cbb9e2ef09e185feb33e3996c99757424
bravin-onwonga <onbravin@gmail.com>
2022-10-25 02:02:12 -0700

    String in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,12 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (s[i] == '\0'; i >= 0; --i)
+	for (i = 0 ; s[i] != '\0'; i++)
 	{
-		_putchar(s[i]);
+		for (i = i -1, i >= 0; i--)
+		{
+			_putchar(s[i]);
+		}
 	}
 	_putchar('\n');
 }
