f831a624b34d800087c8c611cf9d86acf431c52e
bravin-onwonga <onbravin@gmail.com>
2022-10-27 06:24:47 -0700

    Encode the string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *leet - encodes a string into 1337
+ *@str: string to be encoded
+ *
+ *Return: char
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == str1[j])
+				str[i] = str2[j];
+		}
+	}
+	return (str);
+}
