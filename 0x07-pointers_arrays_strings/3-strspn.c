9f26823992992d543b3726d45f5dff55542b9537
bravin-onwonga <onbravin@gmail.com>
2022-11-01 10:23:26 -0700

    Length of prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,16 +12,18 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; s[j] !=  '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (s[j] == accept[i])
 			{
-				count += 1;
+				count ++;
+				break;
 			}
 		}
+		if (accept[i] == '\0')
+			return (count);
 	}
-	count = count + 1;
 	return (count);
 }
