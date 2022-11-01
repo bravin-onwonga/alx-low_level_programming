1e2d6cc2449953d4629a2ae08366c461e1ba16cd
bravin-onwonga <onbravin@gmail.com>
2022-11-01 07:13:45 -0700

    Those that are similar

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ *@s: main string
+ *@accept: substring
+ *
+ *Return: integer of number of similar characters
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count += 1;
+			}
+		}
+	}
+	count = count + 1;
+	return (count);
+}
