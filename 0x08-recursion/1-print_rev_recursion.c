67d4844d4bc5b61c6a042618ed19e5490b44a288
bravin-onwonga <onbravin@gmail.com>
2022-11-03 02:55:46 -0700

    Print in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,12 +10,9 @@
 void _print_rev_recursion(char *s)
 {
 	/*base case*/
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_print_rev_recursion(s);
-	s++;
-	_putchar(*s);
 }
