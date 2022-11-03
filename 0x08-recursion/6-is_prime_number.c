8535b5994d8b2f351985a47503c9c37a54f8def7
bravin-onwonga <onbravin@gmail.com>
2022-11-03 06:04:56 -0700

    Check if n is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *is_prime_number - checks if an integer is a prime number
+ *@n: integer parameter
+ *
+ *Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (-1);
+	if (n == 2)
+		return (1);
+	return (findPrime(n, 2));
+}
+
+/**
+ *findPrime - checks if a is prime
+ *@a: integer
+ *@b: integer
+ *
+ *Return: 1 or -1
+ */
+
+int findPrime(int a, int b)
+{
+	a = a / 2;
+	if (a % b == 0 && a != b)
+	{
+		return (-1);
+	}
+	else
+		return (1);
+	return (findPrime(a, b));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int getSqrt(int x, int y);
+int is_prime_number(int n);i
+int findPrime(int a, int b);
 
 #endif
