96157087e955e7781c93484c758d7b0925115668
bravin-onwonga <onbravin@gmail.com>
2022-11-07 01:29:39 -0800

    Multiplies two integers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - prints the multiplies two numbers
+ *		print error is one number is given
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: 1 if one integer is given otherwise 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	return (0);
+}
