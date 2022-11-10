380ed735eb2ae54a11ee2e86182ebe35326e31f8
bravin-onwonga <onbravin@gmail.com>
2022-11-10 00:50:32 -0800

    First Advanced task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - function to concatenate all arguments passed
+ * @ac: int parameter
+ * @av: double pointer array
+ * Return: pointer to new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, n, k = 0, len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			len++;
+	}
+	len += ac;
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			str[k] = av[i][n];
+			k++;
+		}
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
+	}
+	return (str);
+}
