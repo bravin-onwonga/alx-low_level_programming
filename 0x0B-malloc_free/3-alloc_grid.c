37988ed86053524460f9f1204c2cd6f4522290e3
bravin-onwonga <onbravin@gmail.com>
2022-11-10 00:21:34 -0800

    2dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	m = malloc(sizeof(int *) * width);
+	m = malloc(sizeof(int *) * height);
 	if (m == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
