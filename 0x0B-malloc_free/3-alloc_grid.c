e40f757a0c16060b946a91671261c2be0d32e557
bravin-onwonga <onbravin@gmail.com>
2022-11-10 00:14:05 -0800

    2dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *alloc_grid -  returns a pointer to a 2 dimensional array of integers
+ *@width: number of columns
+ *@height: number of rows
+ *
+ *Return: pointer to array, NULL if height or width is 0 or negative
+ */
+
+int **alloc_grid(int width, int height)
+{
+	char **m;
+	int i, k;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	m = malloc(sizeof(int *) * width);
+	if (m == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		m[i] = malloc(sizeof(int) * width);
+		if (m[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(m[i]);
+			}
+		free(m);
+		return (NULL);
+		}
+	}
+	for (i = 0; j < width; j++)
+		m[i][j] = 0;
+	return (m);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,7 @@
 __0-create_array.c__ - creates an array of chars, and initializes it with a specific char.
 
 __1-strdup.c__ - returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
+__2-str_concat.c__ - concatenates two strings.
+
+__3-alloc_grid.c__ - returns a pointer to a 2 dimensional array of integers.
