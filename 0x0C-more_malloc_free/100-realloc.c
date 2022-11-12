c9d42f688b0ab602db993a4341b286b8325f141b
bravin-onwonga <onbravin@gmail.com>
2022-11-12 11:28:49 -0800

    First advanced task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		clone = ptr;
 	else
 	{
-		return (malloc(newsize));
+		return (malloc(new_size));
 	}
 	if (new_size == old_size)
 		return (ptr);
