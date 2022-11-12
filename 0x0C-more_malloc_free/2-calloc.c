74664d5698f55e4df4350b92678d15fb1b6d9885
bravin-onwonga <onbravin@gmail.com>
2022-11-12 06:19:48 -0800

    Allocates Memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,14 +15,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
 	p = malloc(nmemb * size);
-
 	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < nmemb * size; i++)
 		p[i] = 0;
 
-	retunr (p);
+	return (p);
 }
