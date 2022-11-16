01ab53f04230249548765a407810f20b17f21785
bravin-onwonga <onbravin@gmail.com>
2022-11-15 23:03:39 -0800

    Free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees memory held by dog
+ * @d: pointer to dog to free
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
