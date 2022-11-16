d1c7e412154fb40886297a5b82fd79834986f68a
bravin-onwonga <onbravin@gmail.com>
2022-11-15 22:36:21 -0800

    Define a new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 /*More prototypes*/
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
