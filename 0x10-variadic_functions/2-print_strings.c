bcbf6187c0d5904c8b666d626e5392659a63ad2d
bravin-onwonga <onbravin@gmail.com>
2022-11-18 10:39:54 -0800

    Third task complete

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: string to be printed between strings.
+ * @n: number of string
+ *
+ *Return: Nothing.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list lst;
+	unsigned int i;
+	char *c;
+
+	va_start(lst, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(lst, char *);
+
+		if (str)
+			printf("%s", c);
+		else
+			printf("(nil)");
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(lst);
+}
