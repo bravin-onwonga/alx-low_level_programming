9fa010755852bdee6e9be25b5a8d0a55756673f3
bravin-onwonga <onbravin@gmail.com>
2022-11-18 10:44:51 -0800

    Third task complete

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(lst, char *);
+		ch = va_arg(lst, char *);
 
 		if (ch)
 			printf("%s", ch);
