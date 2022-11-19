515cf1c3db529409f35c7f1c177bc428433696fc
bravin-onwonga <onbravin@gmail.com>
2022-11-19 05:22:55 -0800

    Simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculates the sum of two integers
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: sum
+ */
+
+int op_add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+/**
+ * op_sub - calculates the difference between two integers
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: difference
+ */
+
+int op_sub(int num1, int num2)
+{
+	return (num1 - num2);
+}
+
+/**
+ * op_mul - calculate the product of two numbers
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return:product
+ */
+
+int op_mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * op_div - calculate the division of two integers
+ * @num1: first integer
+ * @num2: second integer
+ *
+ *Return: result from division
+ */
+
+int op_div(int num1, int num2)
+{
+	return (num1 / num2);
+}
+
+/**
+ * op_mod - calculate the remainder from the division of two integers
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: remainder
+ */
+
+int op_mod(int num1, num2)
+{
+	return (num1 % num2);
+}
