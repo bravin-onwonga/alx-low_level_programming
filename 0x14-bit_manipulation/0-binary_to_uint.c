1beea022a818e251746cf17f9691622f2faf22af
bravin-onwonga <onbravin@gmail.com>
2022-12-12 06:49:04 -0800

    Convert to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts binary to decimal
+ * b - string conatinign binary
+ *
+ * Return: decimal number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int num = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		num = 2 * num + (b[i] - '0');
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,5 @@
+##Bit Manipulation##
+
+A project dealing with conversion of binary to hexadecimal, hexadecimal to binary, binary or hexadecimal to decimal.
+
+
