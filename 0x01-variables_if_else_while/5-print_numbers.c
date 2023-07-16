4485f9e98925d7ce68604b86fa526f0a4af5acde
bravin-onwonga <onbravin@gmail.com>
2023-07-16 18:47:50 +0300

    Starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @i - integer
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
