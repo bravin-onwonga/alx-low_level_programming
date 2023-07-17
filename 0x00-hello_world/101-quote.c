064aca74d8dff993273db80f47790e39d92a2b38
bravin-onwonga <onbravin@gmail.com>
2023-07-17 09:13:26 +0300

    Final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main -Entry point
+ *
+ *@i - integer literal
+ *@str - string array
+ *Return: 1 (Sucess)
+ */
+
 int main(void)
 {
 	int i;
-	char str[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		putchar(str[i]);
---
+++ 0x00-hello_world/trial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int b = 4;
+
+	a = 0;
+	for (; a < b;)
+	{
+		printf("%d\n", a++);
+	}
+}
