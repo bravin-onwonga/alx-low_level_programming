5c09eeacd00ec61141603bfd94d7e822804eaa7e
bravin-onwonga <onbravin@gmail.com>
2023-07-19 10:56:03 +0300

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ * @i - integer
+ * @c - character
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+	char c;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
