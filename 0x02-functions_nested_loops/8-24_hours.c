36864e4a1b3a5445faa9fdacc63103b0ff6a7160
bravin-onwonga <onbravin@gmail.com>
2023-07-19 15:48:12 +0300

    Every hour and minute in 24hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,27 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print every hr and min in 24 hrs
+ *
+ * @mins - minutes
+ * @hrs - hours
+ */
 
 void jack_bauer(void)
 {
+	int mins;
+	int hrs;
 
+	for (hrs = 0; hrs <= 23; hrs++)
+	{
+		for (mins = 0; mins <= 59; mins++)
+		{
+			_putchar((hrs / 10) + 48);
+			_putchar((hrs % 10) + 48);
+			_putchar(':');
+			_putchar((mins / 10) + 48);
+			_putchar((mins % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
