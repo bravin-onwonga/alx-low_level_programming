ea2efd200474046cb33cb09ef9c1ec8f30d4d3ad
bravin-onwonga <onbravin@gmail.com>
2023-07-19 13:00:08 +0300

    Last digit of int

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,18 +4,17 @@
  * print_last_digit - prints the last value of int
  *
  * @n: integer
- * @i - last value of n
+ * @lst_digit - last value of n
  * Return: 0 (success)
  */
 
 int print_last_digit(int n)
 {
-	int i;
+	int lst_digit;
 
 	if (n < 0)
 		n = -n;
-	i = n % 10;
+	lst_digit = n % 10;
 
-	_putchar('0' + i);
-	return (0);
+	return (lst_digit);
 }
