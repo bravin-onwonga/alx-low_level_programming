381720fdb8938e49f2fa93980344d89afe30fcb3
bravin-onwonga <onbravin@gmail.com>
2023-07-21 15:56:53 +0300

    Harder than I thought

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,22 +3,17 @@
 int main(void)
 {
 	int i;
-	char str1[] = "Fizz";
-	char str2[] = "Buzz";
-	char str3[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-			printf("%s ", str1);
-		else if (i % 5 == 0 && i % 3 != 0)
-			printf("%s ", str2);
-		else if (i % 15 == 0)
-			printf("%s ", str3);
-		else if (i != 100)
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
 			printf("%d ", i);
-		else if ( i == 100)
-			printf("%d\n", i);
 	}
 	printf("\n");
 	return (0);
