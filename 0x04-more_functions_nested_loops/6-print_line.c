12d8606a1481739546f7fbe190a6b734c20167b7
bravin-onwonga <onbravin@gmail.com>
2023-07-21 11:42:38 +0300

    Using while loop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,19 +4,14 @@
   * print_line - prints '_' n times if n is greater than 0
   *
   * @n: integer
-  * @i - integer
   */
 
 void print_line(int n)
 {
-	int i;
-
-	if (n > 0)
+	while (n >= 0)
 	{
-		for (i = 0; i <= n; i++)
-			_putchar('_');
-		_putchar('\n');
+		_putchar('_');
+		n--;
 	}
-	else
-		_putchar('\n');
+	_putchar('\n');
 }
