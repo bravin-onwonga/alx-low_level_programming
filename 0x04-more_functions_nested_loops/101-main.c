8c1ca3e056415fe16f7d97f253e2ef5478c2879f
bravin-onwonga <onbravin@gmail.com>
2023-07-21 17:10:48 +0300

    First advanced task

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -7,15 +7,15 @@
 	  */
 int main(void)
 {
-	    print_number(98);
+	    print_number(1234567890);
 		    _putchar('\n');
-			    print_number(402);
+			    print_number(2147483647);
 				    _putchar('\n');
-					    print_number(1024);
+					    print_number(-2147483648);
 						    _putchar('\n');
 							    print_number(0);
 								    _putchar('\n');
-									    print_number(-98);
+									    print_number(-00045);
 										    _putchar('\n');
 											    return (0);
 }
