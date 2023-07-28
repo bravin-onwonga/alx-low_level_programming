5cc0bd57b934f742c330f1185fed667ba06ed720
bravin-onwonga <onbravin@gmail.com>
2023-07-28 14:37:30 +0300

    Similar to puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#inlcude "main.h"
+
+/**
+  * _puts - functions similarly to the puts function
+  *
+  * @str: pointer to str
+  * @i - integer
+  */
+
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
