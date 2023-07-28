998141943aba2b1a93361aabbd3a69a59b98b2cc
bravin-onwonga <onbravin@gmail.com>
2023-07-28 13:12:05 +0300

    Checking for spaces

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,15 +5,19 @@
   *
   * @s: pointer to the first character of string
   * @i - integer
+  * @len - holds value of length
   * Return: length of string
   */
 
 int _strlen(char *s)
 {
-	int i;
+	int i, len;
 
 	for (i = 0; *(s + i) != '\0'; i++)
-		i++;
+	{
+		if (*(s + i) != ' ')
+			len++;
+	}
 
-	return (i);
+	return (len);
 }
