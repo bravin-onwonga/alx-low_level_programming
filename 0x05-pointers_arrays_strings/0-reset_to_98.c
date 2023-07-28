74968e3ef67174e3bcc14a2d085a0f10af124bba
bravin-onwonga <onbravin@gmail.com>
2023-07-28 11:54:30 +0300

    First task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - takes an address to int and changes its value to 98
+  * @n: pointer to address of an int
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
