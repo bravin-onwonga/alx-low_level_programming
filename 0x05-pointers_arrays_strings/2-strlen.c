08c90d7149108a9ab5af5f6b6003c2343d7eac40
bravin-onwonga <onbravin@gmail.com>
2023-07-28 13:50:38 +0300

    Claculate a strings len

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,19 +5,15 @@
   *
   * @s: pointer to the first character of string
   * @i - integer
-  * @len - holds value of length
   * Return: length of string
   */
 
 int _strlen(char *s)
 {
-	int i, len;
+	int i;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		if (*(s + i) != ' ')
-			len++;
-	}
+	for (i = 0; s[i] != '\0'; i++)
+		i++;
 
-	return (len);
+	return (i);
 }
