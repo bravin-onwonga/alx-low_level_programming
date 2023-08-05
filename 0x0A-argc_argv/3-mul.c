8c78bfebd81d0d9e34f8beba486b8c7f6a14a883
bravin-onwonga <onbravin@gmail.com>
2023-08-06 01:39:41 +0300

    Multiply two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * main -Entry point
+  *
+  * @argc: argument counter
+  * @argv: argument vector
+  * @res - result of mul argv[1] and argv[2]
+  * Return: 0 success; 1 if arguments are more that two
+  */
+
+int main(int argc, char **argv)
+{
+	int res;
+
+	if (argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	res = argv[1] * argv[2];
+	printf("%d\n", res);
+
+	return (0);
+}
