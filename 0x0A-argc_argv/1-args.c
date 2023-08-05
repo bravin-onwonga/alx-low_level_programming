4043a7c7824794a7a24931e18461bd467c54f126
bravin-onwonga <onbravin@gmail.com>
2023-08-06 01:17:58 +0300

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * @argc: Argument count
+  * @argv: Argument vector
+  * Return: Always 0 (success)
+  */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
