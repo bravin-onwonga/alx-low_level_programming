dae5e61fbf254dd0d6b45a031d7fdc4f8648815f
bravin-onwonga <onbravin@gmail.com>
2023-08-08 10:48:01 +0300

    Checking if size is zero

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ar;
 
-	ar = malloc(sizeof(c) * size);
-
-	if (ar == NULL)
+	if (size == 0)
 		return (NULL);
+
+	ar = malloc(sizeof(c) * size);
 
 	for (i = 0; i < size; i++)
 		ar[i] = c;
-	
+
 	return (ar);
 }
