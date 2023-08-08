936ebe067a760cb88b3086012a7385550e1d3fc0
bravin-onwonga <onbravin@gmail.com>
2023-08-08 10:28:59 +0300

    Saving char to the array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,16 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *ar;
 
-	ar = malloc(size * sizeof(c));
+	ar = malloc(sizeof(c) * size);
 
 	if (ar == NULL)
 		return (NULL);
 
+	for (i = 0; i < size; i++)
+		ar[i] = c;
+	
 	return (ar);
 }
