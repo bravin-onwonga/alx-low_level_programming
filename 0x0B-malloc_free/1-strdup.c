6318ac90f4cdee7b7336ecd9e802cb26b8a6a567
bravin-onwonga <onbravin@gmail.com>
2023-08-08 11:11:12 +0300

    Duplicate a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strdup - duplicates a string
+  *
+  * @str: string to duplicate
+  * Return: pointer to copy of str
+  */
+
+char *_strdup(char *str)
+{
+	int i, l;
+	char *ar;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	ar = malloc(i * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (l = 0; l < i; l++)
+		ar[l] = str[l];
+
+	return (ar);
+}
