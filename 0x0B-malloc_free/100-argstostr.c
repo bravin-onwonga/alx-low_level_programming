c5c9beb782c2a006bb3eb46983d03f658ed4bad3
bravin-onwonga <onbravin@gmail.com>
2023-08-09 10:59:28 +0300

    Print all arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,37 +1,44 @@
 #include "main.h"
 
 /**
+  * argstostr - prints all arguments passed to the program
   *
+  * @ac: argument count
+  * @av: argument vector
+  * Return: double pointer to array; NULL if ac or av is 0
   */
 
 char *argstostr(int ac, char **av)
 {
-	int i, j;
-	char *ar;
+	int i, j, k;
+	char **ar;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
 
-	ar = malloc(ac * sizeof(av));
+	ar = malloc(ac * sizeof(char *));
 
 	if (ar == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		j = 0;
-		while (av[i][j] != '\0')
-		{
-			ar[(i * j)] = av[i][j];
-			j++;
-		}
-		ar[(i * j)] = '\n';
+		for (j = 0; av[i][j] != '\0'; j++)
+			;
+		ar[i] = malloc(j * sizeof(char));
 	}
-	
 
-	return (ar);
+	for (i = 0; i < ac; i++)
+	{
+		k = 0;
+		while (av[i][k] != '\0')
+		{
+			ar[i][k] = av[i][k];
+			k++;
+		}
+		ar[i][k] = '\n';
+	}
+	return (*ar);
 }
-	
-	
