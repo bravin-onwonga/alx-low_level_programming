06b5ff8d9daefc3972ec2c753a808ff0d09a2b19
bravin-onwonga <onbravin@gmail.com>
2023-08-14 17:15:46 +0300

    Initializing memory to zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,6 +10,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	void *ar;
 
 	if (nmemb == 0 || size == 0)
@@ -19,6 +20,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ar == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		ar[i] = 0;
 
 	return (ar);
 }
