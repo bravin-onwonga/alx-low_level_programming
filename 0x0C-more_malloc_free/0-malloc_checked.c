20e4413562dfafee67556571f054fd6dba9d5122
bravin-onwonga <onbravin@gmail.com>
2023-08-14 10:50:33 +0300

    Allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * malloc_checked - allocates memory using malloc
+  *
+  * @b: size of memory
+  * Return: address to memory
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ar;
+
+	ar = malloc(b * sizeof(void *));
+
+	if (ar == NULL)
+	{
+		exit(98);
+	}
+	return (ar);
+}
