8314360f1ca32d0d78b3aef2661a2d2fabb6f054
bravin-onwonga <onbravin@gmail.com>
2023-08-15 23:04:56 +0300

    First struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - stucture for dog
+  *
+  * @name: first member
+  * @age: second member
+  * @owner: third memberi
+  *
+  * Description: struct for variable dog with characteristics
+  *				 name, age and owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
