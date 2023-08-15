374b5fdf67aebfb8889ee4d2ff87182121bfde6c
bravin-onwonga <onbravin@gmail.com>
2023-08-15 23:20:06 +0300

    Initialize a stuct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+  * init_dog - intializes a struct
+  *
+  * @d: pointer to struct dog
+  * @name: value of first member
+  * @age: value of second member
+  * @owner: value of third member
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
