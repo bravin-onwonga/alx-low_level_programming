9d3cc2e5cac3a49942db63fe7e2aee351fbeacf2
bravin-onwonga <onbravin@gmail.com>
2023-08-17 09:40:46 +0300

    Array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+/**
+  * array_iterator - function that iterates through an array
+  *
+  * @array: pointer to array
+  * @size: size of array
+  * @action: function pointer
+  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
