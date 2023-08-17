066756ffb7bdbab9fb242f7a43c1a32ff0f1ccdf
bravin-onwonga <onbravin@gmail.com>
2023-08-17 14:13:24 +0300

    Save copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,6 +12,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dawg;
+	int i, len;
+	char *name_cpy;
+	char *owner_cpy;
 
 	dawg = malloc(sizeof(dog_t));
 
@@ -21,8 +24,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	for (len = 0; name[len] != '\0'; len++)
+		;
+
+	name_cpy = malloc(sizeof(char) * len);
+
+	for (i = 0; i < len; i++)
+	{
+		name_cpy[i] = name[i];
+	}
+
+	for (len = 0; owner[len] != '\0'; len++)
+		;
+
+	owner_cpy = malloc(sizeof(char) * len);
+
+	if owner_cpy == NULL
+	{
+		free(owner_cpy);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		owner_cpy[i] = owner[i];
+	}
+	
 	dawg->name = name;
 	dawg->age = age;
 	dawg->owner = owner;
-	return (dawg);
 }
