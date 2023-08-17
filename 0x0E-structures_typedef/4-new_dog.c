a62d9d15057960428db2fd9d429892ee146d3355
bravin-onwonga <onbravin@gmail.com>
2023-08-17 14:01:13 +0300

    Free memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dawg = malloc(sizeof(dog_t));
 
 	if (dawg == NULL)
+	{
+		free(dawg);
 		return (NULL);
+	}
 
 	dawg->name = name;
 	dawg->age = age;
