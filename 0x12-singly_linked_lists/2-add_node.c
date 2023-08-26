f94061f1dbe22916b23f9c8d871f7c4226d994c5
bravin-onwonga <onbravin@gmail.com>
2023-08-26 10:28:42 +0300

    Add node at beginning

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node - adds node at the beginning of a linked list
+ *
+ * @head: head node (points to first node)
+ * @str: string (first element of node)
+ * Return: pointer to new node
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
+}
