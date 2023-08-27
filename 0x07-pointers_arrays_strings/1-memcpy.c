2410f040d8b155ac3a58a982a3bca83b593dfe8a
bravin-onwonga <onbravin@gmail.com>
2023-08-27 15:22:13 +0300

    Copy a number of characters

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,16 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i, len;
+
+	for (len = 0; dest[len] != '\0'; len++)
+		;
+
+	if (n > len)
+		n = len;
 
 	i = 0;
-	while (i < n && *(src + i) != '\0')
+	while (i < n && src[i] != '\0')
 	{
 		*dest = *src;
 		src++;
