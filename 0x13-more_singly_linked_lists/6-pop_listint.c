9d1ee6792eed0530d215ca6da35865575ee9a90e
bravin-onwonga <onbravin@gmail.com>
2023-08-28 14:21:56 +0300

    Pop an element

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pops the first node of a singly linked list
+ *
+ * @head: pointer to first node
+ * Return: data of the deleted node
+*/
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	temp = *head;
+	free(temp);
+	*head = (*head)->next;
+
+	return (n);
+}
