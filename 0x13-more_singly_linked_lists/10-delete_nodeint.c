d4dab93799a908985f3d1b93edb442c637707339
bravin-onwonga <onbravin@gmail.com>
2023-08-29 22:18:11 +0300

    Delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,6 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	}
 
+	prev = *head;
 	if (!index)
 	{
 		*head = prev->next;
@@ -25,7 +26,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	prev = *head;
 	i = 0;
 	while (i < index - 1)
 	{
