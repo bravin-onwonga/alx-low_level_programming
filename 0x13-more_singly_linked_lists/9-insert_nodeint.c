e1b6e587aadc3c6d2747dc918b741c60a4b3499e
bravin-onwonga <onbravin@gmail.com>
2023-08-29 21:27:07 +0300

    Insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,8 +36,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp->next = new;
 			return (new);
 		}
-		temp = temp->next;
 		i++;
+		temp = temp->next;
 	}
 	free(new);
 	return (NULL);
