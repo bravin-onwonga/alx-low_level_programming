1ef6ae92292a7eeb8e7e8e2e07df11b54a7fc510
bravin-onwonga <onbravin@gmail.com>
2023-08-29 22:03:39 +0300

    Insert node at index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node at index
+ *
+ * @head: pointer to first node
+ * @index: index of node to delete
+ * Return: 1 (success) ; otherwise -1
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *prev;
+	unsigned int i;
+
+	i = 0;
+	if (head == NULL)
+	{
+		return (-1);
+	}
+	prev = *head;
+	if (!index)
+	{
+		*head = prev->next;
+		free(prev);
+		return(1);
+	}
+	while (i < index - 1)
+	{
+		if (prev == NULL)
+			return (-1);
+		prev = prev->next;
+		i++;
+	}
+
+	temp = prev->next->next;
+	free(prev->next);
+	prev->next = temp;
+	return (1);
+}
