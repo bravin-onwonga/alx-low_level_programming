012dd2026ecbb9584328cfa5e476b9f51c3e743b
bravin-onwonga <onbravin@gmail.com>
2023-08-29 09:20:10 +0300

    Add node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
 	if (*head == NULL && idx == 0)
 	{
 		new->n = n;
