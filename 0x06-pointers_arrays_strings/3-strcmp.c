6c684a922c337418b0616db89c986330480d32e6
bravin-onwonga <onbravin@gmail.com>
2023-08-30 17:21:39 +0300

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,8 +5,6 @@
   *
   * @s1: first string
   * @s2: second string
-  * @len_s1 - length of s1
-  * @len_s2 - length of s2
   * Return: 15 if s1 is longer, -15 if s2 is longer and 0 if same
   */
 
@@ -15,13 +13,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int len_s1, len_s2;
 
-	len_s1 = 0;
-	while (*(s1 + len_s1) != '\0')
-		len_s1++;
+	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
+		;
 
 	len_s2 = 0;
-	while (*(s2 + len_s2) != '\0')
-		len_s2++;
+	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
+		;
 
 	if (len_s1 > len_s2)
 		return (15);
