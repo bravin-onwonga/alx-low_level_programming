2c69413cbe67e0b5b685ce5ca2bc16624b652042
bravin-onwonga <onbravin@gmail.com>
2023-08-30 12:31:36 +0300

    Largest prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,6 +1,55 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+ * find_largestprime - finds the largest  divisible prime number
+ *
+ * @n: long integer
+ * Return: largest prime number
+*/
+
+long int find_largestprime(long int n)
+{
+	long int largest = -1, i;
+
+	while (n % 2 == 0)
+	{
+		largest = 2;
+		n /= 2;
+	}
+
+	if (n == 2 || n == 3)
+		return (n);
+
+	for(i = 3; i * i <= n; i += 2)
+	{
+		while (n % i == 0)
+		{
+			largest = i;
+			n /= i;
+		}
+	}
+
+	if (n > 2)
+		largest = n;
+
+	return (largest);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+*/
 
 int main(void)
 {
+	long int num,largest_prime;
+
+	num = 612852475143;
+
+	largest_prime = find_largestprime(num);
+
+	printf("%ld\n", largest_prime);
+
 	return (0);
 }
