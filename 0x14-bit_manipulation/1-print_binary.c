68dec296d1c4da3cf70e424e544e388598574222
bravin-onwonga <onbravin@gmail.com>
2023-09-02 09:57:59 +0300

    Int to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+void printInt(unsigned long int num)
+{
+	if (num / 10)
+	{
+		printInt(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
+
 void print_binary(unsigned long int n)
 {
 	int j, i = 16;
@@ -9,6 +18,11 @@ void print_binary(unsigned long int n)
 	if (n == 0)
 	{
 		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
 		return;
 	}
 
@@ -29,6 +43,14 @@ void print_binary(unsigned long int n)
 		}
 		if (n == 1)
 		{
+			if (i != 0)
+			{
+				while(i)
+				{
+					_putchar('0');
+					i--;
+				}
+			}
 			_putchar('1');
 			return;
 		}
