8d4363a4c208553464cf6ac1499e55bc3e499e1e
bravin-onwonga <onbravin@gmail.com>
2023-09-12 00:18:36 +0300

    Copy from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,21 +23,23 @@ int main(int argc, char *argv[])
 int cp_file_from_file_to(const char *file_from, const char *file_to)
 {
 	int fd;
-	int FD;
+	FILE *fp = fopen(file_from, "rb");
 	ssize_t bytes_written, read_bytes;
-	char *buffer = malloc(sizeof(file_from));
+	size_t size = 0;
+	char *buffer[1024];
 
-	fd = open(file_to, O_CREAT | O_WRONLY | O_APPEND, 0664);
-
-	if (fd == -1)
+	if (fp == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
 
-	FD = open(file_from, O_RDONLY);
+	read_bytes = fread(buffer, STDOUT_FILENO, sizeof(buffer), fp);
 
-	read_bytes = read(FD, buffer, sizeof(file_from));
+	if (read_bytes > 0)
+	{
+		size += read_bytes;
+	}
 
 	if (read_bytes == -1)
 	{
@@ -45,7 +47,15 @@ int cp_file_from_file_to(const char *file_from, const char *file_to)
 		exit(98);
 	}
 
-	bytes_written = write(fd, buffer, sizeof(buffer));
+	fd = open(file_to, O_CREAT | O_RDWR | O_APPEND, 0664);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	bytes_written = write(fd, buffer, size);
 
 	if (bytes_written == -1)
 	{
@@ -53,7 +63,7 @@ int cp_file_from_file_to(const char *file_from, const char *file_to)
 		exit(99);
 	}
 
-	close(FD);
+	fclose(fp);
 	close(fd);
 	return (0);
 }
