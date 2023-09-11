c9da5b173fa244e3fc98dd9c2b5039fef16e91aa
bravin-onwonga <onbravin@gmail.com>
2023-09-11 10:48:49 +0300

    Write to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,9 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t bytes_written;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
