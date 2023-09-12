40ded78df522e69220833328523cb86b717a08f9
bravin-onwonga <onbravin@gmail.com>
2023-09-12 17:13:15 +0300

    Copy from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (sucess)
+*/
+
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
@@ -20,6 +28,15 @@ int main(int argc, char *argv[])
 
 }
 
+/**
+ * cp_file_from_file_to - copies contents of fil_from to file_to
+ * creates file_to if it doesn't exits permission being rw-rw-r--
+ *
+ * @file_from: file with content to copy
+ * @file_to: file to copy to | Truncate if exists
+ * Return: 0 (sucess)
+*/
+
 int cp_file_from_file_to(const char *file_from, const char *file_to)
 {
 	int fd;
@@ -35,28 +52,25 @@ int cp_file_from_file_to(const char *file_from, const char *file_to)
 	}
 
 	read_bytes = fread(buffer, STDOUT_FILENO, sizeof(buffer), fp);
-
-	if (read_bytes > 0)
-	{
-		size += read_bytes;
-	}
-
 	if (read_bytes == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	fd = open(file_to, O_TRUNC | O_RDWR | O_CREAT, 0664);
+	if (read_bytes > 0)
+	{
+		size += read_bytes;
+	}
 
+	fd = open(file_to, O_TRUNC | O_RDWR | O_CREAT, 0664);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 
-	bytes_written = write(fd, buffer, size);
-
+	bytes_written = write(fd, buffer, (size - 1));
 	if (bytes_written == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
@@ -65,5 +79,11 @@ int cp_file_from_file_to(const char *file_from, const char *file_to)
 
 	fclose(fp);
 	close(fd);
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
+		exit(100);
+	}
+
 	return (0);
 }
