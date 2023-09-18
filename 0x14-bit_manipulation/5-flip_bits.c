5d5bb14fef5e2d2f36b6b676b12a17468caaaf7c
bravin-onwonga <onbravin@gmail.com>
2023-09-18 19:00:58 +0300

    Flip the bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -34,7 +34,7 @@ unsigned int count_extra(unsigned long int num)
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0, num;
-	int i = 1;
+	int i = 0;
 
 	if ((n & 1) != (m & 1))
 	{
