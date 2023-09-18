854c50471832e811546267a517fcc9ae96dca7d9
bravin-onwonga <onbravin@gmail.com>
2023-09-18 18:39:30 +0300

    Flip the bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * count_extra - counts the extra bits after num is zero
+ *
+ * @num: integer
+ * Return: number of extra bits that are set
+*/
+
+unsigned int count_extra(unsigned long int num)
+{
+	unsigned int count = 0;
+
+	while (num != 0)
+	{
+		if (num >> 1)
+		{
+			count++;
+		}
+		num = num >> 1;
+	}
+	return (count);
+}
+
+/**
  * flip_bits - checks how many bits you need to flip
  * to get another number
  *
@@ -11,7 +33,7 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count = 0;
+	unsigned int count = 0, num;
 	int i = 0;
 
 	if ((n & 1) != (m & 1))
@@ -21,13 +43,24 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	while (i < 64)
 	{
+		if (n == 0 && m != 0)
+		{
+			num = count_extra(m);
+			count += num;
+			break;
+		}
+		if (m == 0 && n != 0)
+		{
+			num = count_extra(n);
+			count += num;
+			break;
+		}
 		if ((n >> 1) != (m >> 1))
 		{
 			count++;
 		}
 		n = n >> 1;
 		m = m >> 1;
-
 		i++;
 	}
 	return (count);
