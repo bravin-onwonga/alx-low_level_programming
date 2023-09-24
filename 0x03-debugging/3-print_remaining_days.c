990100eceab73621f7759fd73be7553c8887b8ff
bravin-onwonga <onbravin@gmail.com>
2023-09-24 20:06:42 +0300

    Print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,11 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (year % 100 == 0 && year % 400 == 0)
 		{
-			if (month >= 2 && day > 60)
+			if (month == 2 && day == 60)
+			{
+				;
+			}
+			if (month > 2 && day >= 60)
 			{
 				day++;
 			}
@@ -23,7 +27,11 @@ void print_remaining_days(int month, int day, int year)
 
 		else if (year % 4 == 0 && year % 400 != 0)
 		{
-			if (month >= 2 && day > 60)
+			if (month == 2 && day == 60)
+			{
+				;
+			}
+			if (month >= 2 && day >= 60)
 			{
 				day++;
 			}
