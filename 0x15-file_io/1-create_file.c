8a59e83fc61ccf1b6dd61d35f6a9377d3916b820
bravin-onwonga <onbravin@gmail.com>
2023-10-02 12:13:09 +0300

    File Input/Output

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file with 0600 permission
+ * if it didn't exists and truncates it if it did
+ *
+ * @filename: file to create
+ * @text_content: content to copy to filename
+ *
+ * Return: 1 (success); otherwise -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
+	bytes_written = write(fd, text_content, strlen(text_content));
+
+	if (bytes_written == -1)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to a file
+ * do not create if file doesn't exist,
+ *
+ * @filename: file to append content to
+ * @text_content: content to append
+ * Return: 1 (success); otherwise -1
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
+	bytes_written = write(fd, text_content, strlen(text_content));
+
+	if (bytes_written == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
