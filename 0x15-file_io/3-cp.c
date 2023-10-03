e9ce44a24c457563f803221e6023188fc78c8b84
bravin-onwonga <onbravin@gmail.com>
2023-10-03 22:36:01 +0300

    Solving test fails

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int cp_file_from_file_to(const char *file_to, char *text_content, size_t size);
-
 /**
  * main - Entry point
  *
@@ -12,11 +10,6 @@ int cp_file_from_file_to(const char *file_to, char *text_content, size_t size);
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	char buffer[1024];
-	size_t size = 0;
-	ssize_t read_bytes;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -29,43 +22,46 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fp = fopen(argv[1], "rb");
-	if (fp == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	cp_file_from_file_to(argv[1], argv[2]);
 
-	read_bytes = fread(buffer, STDOUT_FILENO, sizeof(buffer), fp);
-	if (read_bytes <= 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (read_bytes > 0)
-	{
-		size += read_bytes;
-	}
-
-	cp_file_from_file_to(argv[2], buffer, size);
-	fclose(fp);
 	return (0);
+
 }
 
 /**
  * cp_file_from_file_to - copies contents of fil_from to file_to
  * creates file_to if it doesn't exits permission being rw-rw-r--
  *
- * @text_content: file with content to copy
+ * @file_from: file with content to copy
  * @file_to: file to copy to | Truncate if exists
- * @size: number of bytes in text_content
  * Return: 0 (sucess)
 */
 
-int cp_file_from_file_to(const char *file_to, char *text_content, size_t size)
+int cp_file_from_file_to(const char *file_from, const char *file_to)
 {
 	int fd;
-	ssize_t bytes_written;
+	FILE *fp = fopen(file_from, "rb");
+	ssize_t bytes_written, read_bytes;
+	size_t size = 0;
+	char *buffer[1024];
+
+	if (fp == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	read_bytes = fread(buffer, STDOUT_FILENO, sizeof(buffer), fp);
+	if (read_bytes <= 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	if (read_bytes > 0)
+	{
+		size += read_bytes;
+	}
 
 	fd = open(file_to, O_TRUNC | O_RDWR | O_CREAT, 0664);
 	if (fd == -1)
@@ -74,18 +70,20 @@ int cp_file_from_file_to(const char *file_to, char *text_content, size_t size)
 		exit(99);
 	}
 
-	bytes_written = write(fd, text_content, size);
+	bytes_written = write(fd, buffer, size);
 	if (bytes_written == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 
-	close(fd);
-	if (close(fd) != -1)
+	fclose(fp);
+	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
 		exit(100);
 	}
+
+
 	return (0);
 }
