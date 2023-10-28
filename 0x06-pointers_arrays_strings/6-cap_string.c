d868d83dc9561ab960e9a8f33cb2c9cc2bd34b9f
bravin-onwonga <onbravin@gmail.com>
2023-10-28 19:18:37 +0300

    Capitize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
+/**
+ * cap_string - capitizes a string
+ *
+ * @s: string
+ * Return: pointer to capitized string
+ */
+
 char *cap_string(char *s)
 {
 	int i, j;
 	char a[12] = {',', '.', '\t', '\n', '!', '?', '"', '(', ')', '{', '}', ' '};
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] = s[0] - 'a' + 'A';
+	}
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
