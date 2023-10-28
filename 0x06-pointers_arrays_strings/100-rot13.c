ee4bc64a1e32f7dc783932ee862daa53121fb2de
bravin-onwonga <onbravin@gmail.com>
2023-10-28 23:02:56 +0300

    Encodes string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ *
+ * @s: string
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int index, i;
+	char arr1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char arr2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		index = 0;
+		while (index < 52)
+		{
+			if (s[i] == arr1[index])
+			{
+				s[i] = arr2[index];
+				break;
+			}
+			index++;
+		}
+		i++;
+	}
+	return (s);
+}
