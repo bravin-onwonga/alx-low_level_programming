fa6fb2d69715923a0921fcb824f284940335e4ff
bravin-onwonga <onbravin@gmail.com>
2023-10-30 21:36:53 +0300

    Concatenate a string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+unsigned int str_len(char *str);
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int len_s1, len_s2, k, i;
+	char *ar;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	len_s1 = str_len(s1);
+	len_s2 = str_len(s2);
+
+	ar = malloc(sizeof(char) * (len_s1 + len_s2) - 1);
+
+	if (ar == NULL)
+		return (NULL);
+
+	while (i < (len_s1 - 1))
+	{
+		ar[i] = s1[i];
+		i++;
+	}
+
+	k = 0;
+	while (k < len_s2)
+	{
+		ar[i] = s2[k];
+		i++;
+		k++;
+	}
+
+	return (ar);
+}
+
+/**
+ * str_len - calculates lenght of a string
+ *
+ * @str: string
+ * Return: length of string
+ */
+
+unsigned int str_len(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+
+	return (i);
+}
