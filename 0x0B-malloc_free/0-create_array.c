80cba3f1f9d2007834a279a5211a063cbbfa0e60
bravin-onwonga <onbravin@gmail.com>
2023-10-30 19:58:44 +0300

    Create array using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_array - creates and intializes an array using malloc
+ *
+ * @size: size of array
+ * @c: character to be member of array
+ * Return: pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ar;
+
+	ar = malloc(sizeof(char) * size);
+
+	i = 0;
+	while (i < size)
+	{
+		ar[i] = c;
+		i++;
+	}
+
+	return (ar);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+## Project on memory allocation -malloc and free
+
+### Files in this project
+- **0-create_array.c** - contains a function which takes an integer and character as argument and initalizes an array of int length with the given character as members.
