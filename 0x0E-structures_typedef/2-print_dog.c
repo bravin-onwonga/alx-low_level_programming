852ab71294d5796bfc988f6c3ac2635101e402d9
bravin-onwonga <onbravin@gmail.com>
2023-11-06 17:03:33 +0300

    Wrong printf text

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,10 +16,10 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	printf("Name: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Name: (nil)\n");
+		printf("Owner: (nil)\n");
 	else
-		printf("Name: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 }
