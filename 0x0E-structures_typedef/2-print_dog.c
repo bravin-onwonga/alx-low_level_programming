fb71c287b4c78dd62d67d659d2169b2722479d95
bravin-onwonga <onbravin@gmail.com>
2023-11-06 16:00:44 +0300

    Print a struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct
+ *
+ * @d: struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Name: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->owner);
+}
