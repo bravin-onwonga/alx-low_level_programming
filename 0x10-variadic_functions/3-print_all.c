924f7621de396eb95c08ad66a016fbd422ab7deb
bravin-onwonga <onbravin@gmail.com>
2023-11-09 12:59:40 +0300

    Print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -29,7 +29,7 @@ void print_all(const char *const format, ...)
 			str = va_arg(ap, char *);
 			if (str == NULL)
 				printf("(nil)");
-			else
+			if (str != NULL)
 				printf("%s", str);
 			break;
 		case 'c':
