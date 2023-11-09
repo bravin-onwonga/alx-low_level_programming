f993c220e4964f6c51bef5bc7985b0dfe1d45dc8
bravin-onwonga <onbravin@gmail.com>
2023-11-09 13:36:08 +0300

    Solving for when NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,19 +11,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list(ap);
 	unsigned int i;
+	char *str;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(ap, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 		if (i < n - 1)
 		{
-			if (separator != NULL)
-				printf("%s%s", va_arg(ap, char *), separator);
-			else
-				printf("%s", va_arg(ap, char *));
+			printf("%s", separator);
 		}
-		else
-			printf("%s", va_arg(ap, char *));
 	}
 	printf("\n");
 	va_end(ap);
