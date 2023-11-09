1b2106409d1a14ea9b03374b67c18129ddbb58b4
bravin-onwonga <onbravin@gmail.com>
2023-11-09 10:52:45 +0300

    Changes to printing separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d%c ", va_arg(ap, int), separator[0]);
+		if (separator != NULL)
+			printf("%d%s", va_arg(ap, int), separator);
+		printf("%d ", va_arg(ap, int));
 	}
 	printf("%d\n", va_arg(ap, int));
 	va_end(ap);
