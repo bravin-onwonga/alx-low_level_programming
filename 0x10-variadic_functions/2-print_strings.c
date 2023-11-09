20dea1e451824b0d1bfabd8b2cf3f9a374dfa661
bravin-onwonga <onbravin@gmail.com>
2023-11-09 13:27:32 +0300

    Adding new line character

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,13 +13,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
-			printf("%s%s", va_arg(ap, char *), separator);
+		if (i < n - 1)
+		{
+			if (separator != NULL)
+				printf("%s%s", va_arg(ap, char *), separator);
+			else
+				printf("%s", va_arg(ap, char *));
+		}
 		else
-			printf("%s ", va_arg(ap, char *));
+			printf("%s", va_arg(ap, char *));
 	}
-	printf("%s", va_arg(ap, char *));
+	printf("\n");
 	va_end(ap);
 }
