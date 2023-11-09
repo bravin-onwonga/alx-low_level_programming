1a1f995b732689b228a24908432c610673b1e519
bravin-onwonga <onbravin@gmail.com>
2023-11-09 15:56:17 +0300

    Checking if format is NULL

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,28 +15,31 @@ void print_all(const char *const format, ...)
 	va_start(ap, format);
 
 	p = format;
-	while (*p != '\0')
+	if (p)
 	{
-		switch (*p)
+		while (*p != '\0')
 		{
-		case 'i':
-			printf("%s%d", sep, va_arg(ap, int));
-			break;
-		case 'f':
-			printf("%s%f", sep, va_arg(ap, double));
-			break;
-		case 's':
-			str = va_arg(ap, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", sep, str);
-			break;
-		case 'c':
-			printf("%s%c", sep, va_arg(ap, int));
-			break;
+			switch (*p)
+			{
+			case 'i':
+				printf("%s%d", sep, va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			case 'c':
+				printf("%s%c", sep, va_arg(ap, int));
+				break;
+			}
+			sep = ", ";
+			p++;
 		}
-		sep = ", ";
-		p++;
 	}
 	printf("\n");
 	va_end(ap);
