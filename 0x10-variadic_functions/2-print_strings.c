35f19e9f3b8255e2a063715ca5938f1315491255
bravin-onwonga <onbravin@gmail.com>
2023-11-09 13:37:19 +0300

    Solving for when NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		if (i < n - 1)
+		if (i < (n - 1))
 		{
 			printf("%s", separator);
 		}
