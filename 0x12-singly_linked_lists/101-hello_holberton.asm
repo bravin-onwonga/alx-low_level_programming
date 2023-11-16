d4708ffb7c78de63609aa51821ab530259daaa49
bravin-onwonga <onbravin@gmail.com>
2023-11-16 15:56:39 +0300

    First program in assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+    holberton_str db 'Hello, Holberton', 0xA
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, holberton_str
+    mov rdx, 17
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
