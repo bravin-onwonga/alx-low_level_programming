65e836c1b088580f53dc6e88666f61dc18fa27f4
bravin-onwonga <onbravin@gmail.com>
2023-11-20 14:02:12 +0300

    Solving test case error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+void free_listint(listint_t *head);
+
 /**
  * free_listint2 - frees a singly linked list
  *		and sets head to NULL
@@ -9,6 +11,24 @@
 
 void free_listint2(listint_t **head)
 {
-	free_listint(head);
+	free_listint(*head);
 	*head = NULL;
+}
+
+/**
+ * free_listint - frees a singly linked list
+ * @head: head node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	free(head);
 }
