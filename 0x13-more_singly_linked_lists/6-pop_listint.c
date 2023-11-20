63b387ac487257f248d7b9e6ad559a587f4d0db5
bravin-onwonga <onbravin@gmail.com>
2023-11-20 14:04:21 +0300

    Delete first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes first node
+ *
+ * @head: head node
+ * Return: address of new first node
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	temp = *head;
+	n = temp->n;
+	temp = temp->next;
+	free(*head);
+	*head = temp;
+
+	return (n);
+}
