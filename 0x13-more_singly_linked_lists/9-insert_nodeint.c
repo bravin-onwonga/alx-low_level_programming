59cae33df0424016aac9cb980fd8d7e8f205bce5
bravin-onwonga <onbravin@gmail.com>
2023-11-21 10:50:05 +0300

    Solving test case errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,27 +14,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev, *curr, *new_node;
 	unsigned int i = 0;
 
+	if (!head)
+		return (NULL);
+
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
-	curr = *head;
-	while (curr != NULL)
-	{
-		if (i == idx)
-		{
-			new_node->n = n;
-			new_node->next = curr;
-			prev->next = new_node;
+	new_node->n = n;
 
-			return (new_node);
-		}
+	if (*head == NULL && idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	curr = *head;
+	while (i < idx)
+	{
+		if (!curr)
+			return (NULL);
 		prev = curr;
 		curr = curr->next;
 		i++;
 	}
-	free(new_node);
+	new_node->next = prev->next;
+	prev->next = new_node;
 
-	return (NULL);
+	return (new_node);
 }
