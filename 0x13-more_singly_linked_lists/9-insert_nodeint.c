4c4ef987353701ea808f67484eadf6709b0fbdfa
bravin-onwonga <onbravin@gmail.com>
2023-11-21 12:11:40 +0300

    Freeing new node if fail

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i < idx)
 	{
 		if (curr == NULL)
+		{
+			free(new_node);
 			return (NULL);
+		}
 		curr = curr->next;
 		i++;
 	}
