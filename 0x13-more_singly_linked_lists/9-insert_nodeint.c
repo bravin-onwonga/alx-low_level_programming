70e74f64684369bfa3ff8b8e5a47205c3fa00452
bravin-onwonga <onbravin@gmail.com>
2023-11-21 12:00:10 +0300

    Inserting at 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	if (*head == NULL && idx == 0)
+	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i < idx)
 	{
 		if (curr == NULL)
-			break;
+			return (NULL);
 		curr = curr->next;
 		i++;
 	}
