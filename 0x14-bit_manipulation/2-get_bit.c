4ad31fcef1eca8020e4cb6f99193128dcaf8dac1
bravin-onwonga <onbravin@gmail.com>
2023-11-23 12:06:51 +0300

    Bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - get the bit at an index
+ *
+ * @n: integer
+ * @index: index of bit required
+ * Return: 1 if bit is '1' or '0'
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+
+	while (index > 0)
+	{
+		n >>= 1;
+		if (n == 0)
+			return (-1);
+		index--;
+	}
+
+	return (n & 1);
+}
