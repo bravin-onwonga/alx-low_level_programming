286a7d75eb270b6794d5eca8602db44f9bcb5f82
bravin-onwonga <onbravin@gmail.com>
2023-11-23 16:49:29 +0300

    Set bit at index to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+unsigned long int binary_to_ulint(char *b);
+
 /**
  * set_bit - sets bit at index to one
  *
@@ -10,15 +12,78 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	ulint m;
+	ulint num, temp;
+	int i;
+	char *bitarr;
+
+	temp = *n;
 
 	if (index > 32)
 		return (-1);
 
-	m = 1;
-	m <<= index;
+	i = 31;
+	bitarr = malloc(sizeof(char) * 32);
 
-	*n = *n | m;
+	while (i >= 0)
+	{
+		bitarr[i] = (temp & 1 ? '1' : '0');
+		temp >>= 1;
+		i--;
+	}
 
+	bitarr[31 - index] = '1';
+	num = binary_to_ulint(bitarr);
+
+	*n = num;
+
+	free(bitarr);
 	return (0);
+}
+
+/**
+ * binary_to_ulint - converts binary to int
+ *
+ * @b: pointer to binary string
+ * Return: converted int (success); otherwise 0
+ */
+
+unsigned long int binary_to_ulint(char *b)
+{
+	unsigned long int n;
+	int len = 31, i, j, k;
+	unsigned long int value;
+
+	n = 0;
+
+	if (b == NULL)
+		return (0);
+
+	i = len;
+	k = 0;
+	while (k <= len && i >= 0)
+	{
+		if (b[i] != '1' && b[i] != '0')
+		{
+			return (0);
+		}
+		if (b[i] == '1' && k == 0)
+		{
+			n = n + 1;
+		}
+
+		else if (b[i] == '1' && k != 0)
+		{
+			value = 1;
+			j = k;
+			while (j > 0)
+			{
+				value = value * 2;
+				j--;
+			}
+			n = n + value;
+		}
+		k++;
+		i--;
+	}
+	return (n);
 }
