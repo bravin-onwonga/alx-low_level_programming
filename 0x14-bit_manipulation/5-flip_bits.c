13a72c2f035331b332c2aa3bfc7e3696a4811eaf
bravin-onwonga <onbravin@gmail.com>
2023-11-23 19:53:24 +0300

    Checking first bit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,10 +13,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int countBits_n = 0, countBits_m = 0, i, count = 0;
 
+	if (n & 1 != m & 1)
+		count++;
+
 	for (i = 0; i <= 32; i++)
 	{
-		countBits_n = (n & 1 ? 1 : 0);
-		countBits_m = (m & 1 ? 1 : 0);
+		countBits_n = (n >> 1 & 1 ? 1 : 0);
+		countBits_m = (m >> 1 & 1 ? 1 : 0);
 		n >>= 1;
 		m >>= 1;
 
