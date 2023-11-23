b8bcad6c5bb446991178a42b86bdfd5d52621a2e
bravin-onwonga <onbravin@gmail.com>
2023-11-23 15:48:59 +0300

    Converting to char then to ul int

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+unsigned int binary_to_ulint(char *b);
+
 /**
  * clear_bit - sets bit at index to 0
  *
@@ -10,24 +12,76 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	ulint value;
-	unsigned int i;
+	ulint num, temp;
+	int i;
+	char *bitarr;
+
+	temp = *n;
 
 	if (index > 32)
 		return (-1);
 
-	i = 0;
-	value = 1;
-	while (i < index)
+	i = 31;
+	bitarr = malloc(sizeof(char) * 32);
+
+	while (i >= 0)
 	{
-		value = value * 2;
-		i++;
+		bitarr[i] = (temp & 1 ? '1' : '0');
+		temp >>= 1;
+		i--;
 	}
 
-	if (value > *n)
-		*n = 0;
+	bitarr[31 - index] = '0';
+	num = binary_to_ulint(bitarr);
 
-	else
-		*n -= value;
-	return (1);
+	*n = num;
+	return (0);
+}
+
+/**
+ * binary_to_ulint - converts binary to int
+ *
+ * @b: pointer to binary string
+ * Return: converted int (success); otherwise 0
+ */
+
+unsigned int binary_to_ulint(char *b)
+{
+	unsigned long int n;
+	int len = 31, i, j, k;
+	int value;
+
+	n = 0;
+
+	if (b == NULL)
+		return (0);
+
+	i = len;
+	k = 0;
+	while (k <= len && i >= 0)
+	{
+		if (b[i] != '1' && b[i] != '0')
+		{
+			return (0);
+		}
+		if (b[i] == '1' && k == 0)
+		{
+			n = n + 1;
+		}
+
+		else if (b[i] == '1' && k != 0)
+		{
+			value = 1;
+			j = k;
+			while (j > 0)
+			{
+				value = value * 2;
+				j--;
+			}
+			n = n + value;
+		}
+		k++;
+		i--;
+	}
+	return (n);
 }
