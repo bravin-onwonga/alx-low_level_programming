88985ab1adc132d858873c30031febe8cb8bf05a
bravin-onwonga <onbravin@gmail.com>
2023-11-27 20:53:25 +0300

    Checking fails independently

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+int cp_file_from_file_to(char *file_from, char *file_to);
+void check_fails(int file_state, int fd, char *file_name, char action_tried);
+
 /**
  * get_strlen - finds length of string
  *
@@ -17,7 +20,13 @@ size_t get_strlen(char *s)
 	return (i);
 }
 
-int cp_file_from_file_to(char *file_from, char *file_to);
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success); else exit 98 can't read first argument or is null
+ */
 
 int main(int argc, char *argv[])
 {
@@ -44,9 +53,18 @@ int main(int argc, char *argv[])
 	return (res);
 }
 
+/**
+ * cp_file_from_file_to - does the copying to a file from buffer
+ *
+ * @file_to: file to copy to
+ * @file_from: number of characters read
+ * Return: 0 (success); exit 99 can't write to second argument
+ *			exit 100 can't close file descriptor
+ */
+
 int cp_file_from_file_to(char *file_from, char *file_to)
 {
-	int fd, fd_txt;
+	int fd, fd_txt, close_fd, close_fdtxt;
 	mode_t mode = 0664;
 	ssize_t bytes_read, bytes_written;
 	char *buf;
@@ -63,11 +81,8 @@ int cp_file_from_file_to(char *file_from, char *file_to)
 	buf = malloc((get_strlen(file_from) * sizeof(char *)));
 	if (buf == NULL || fd_txt == -1)
 	{
-		close(fd);
-		close(fd_txt);
 		free(buf);
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
+		check_fails(-1, -1, file_from, 'R');
 	}
 
 	while ((bytes_read = read(fd_txt, buf, 1024)) > 0)
@@ -75,20 +90,49 @@ int cp_file_from_file_to(char *file_from, char *file_to)
 		bytes_written = write(fd, buf, bytes_read);
 		if (bytes_written == -1)
 		{
-			close(fd);
-			close(fd_txt);
 			free(buf);
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			exit(99);
+			check_fails(-1, -1, file_to, 'W');
 		}
 	}
 
-	free(buf);
-	close(fd_txt);
-	if (close(fd) != 0)
+	if (bytes_read == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
+		free(buf);
+		check_fails(-1, -1, file_from, 'R');
+	}
+	close_fdtxt = close(fd_txt);
+	check_fails(close_fdtxt, fd_txt, NULL, 'C');
+	close_fd = close(fd);
+	check_fails(close_fd, fd, NULL, 'C');
+	return (0);
+}
+
+/**
+ * check_fails - checks for fails in read, write and close
+ *
+ * @file_state: state of file
+ * @fd: int value of file descriptor
+ * @file_name: name of file being read or written to
+ * @action_tried: can be Read(R), Write(W) or Close(C)
+ */
+
+void check_fails(int file_state, int fd, char *file_name, char action_tried)
+{
+	if (action_tried == 'W' && file_state == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_name);
+		exit(99);
+	}
+
+	if (action_tried == 'R' && file_state == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_name);
+		exit(98);
+	}
+
+	if (action_tried == 'R' && file_state == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	return (0);
 }
