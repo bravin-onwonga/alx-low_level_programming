e962cfb7a44b0d9cbec6673bbe4a364d76cd4117
bravin-onwonga <onbravin@gmail.com>
2024-01-11 21:20:15 +0300

    First node's previous value is NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,19 +22,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->n = n;
 
-	if (*head == NULL)
-	{
-		new_node->next = NULL;
-		new_node->prev = head;
-		*head = new_node;
-	}
-
-	else
-	{
-		new_node->next = *head;
-		*head = new_node;
-		new_node->prev = *head;
-	}
+	new_node->next = *head;
+	*head = new_node;
+	new_node->prev = NULL;
 
 	return (*head);
 }
