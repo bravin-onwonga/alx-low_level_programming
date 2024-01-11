f9119e8ff120f5ffb0ed1fe27096423ff7c09629
bravin-onwonga <onbravin@gmail.com>
2024-01-12 02:06:39 +0300

    Insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+dlistint_t *handle_idx_zero(dlistint_t **h, dlistint_t *new_node, int n);
+
 /**
  * insert_dnodeint_at_index - inserts a node at a given index
  *
@@ -22,23 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n;
-
-	if (*h == NULL && idx == 0)
-	{
-		new_node->next = *h;
-		*h = new_node;
-		new_node->prev = NULL;
-	}
-
-	else if (*h != NULL && idx == 0)
-	{
-		temp = *h;
-		temp->prev = new_node;
-		new_node->next = *h;
-		*h = new_node;
-		new_node->prev = NULL;
-	}
+	if (idx == 0)
+		new_node = handle_idx_zero(h, new_node, n);
 
 	else
 	{
@@ -51,12 +38,48 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			i++;
 		}
 
+		new_node->n = n;
+
 		temp = curr->next;
 		curr->next = new_node;
 		new_node->next = temp;
 		new_node->prev = curr;
 		if (temp)
 			temp->prev = new_node;
+	}
+
+	return (new_node);
+}
+
+/**
+ * handle_idx_zero - handles for when index is zero
+ *
+ * @h: address of head node
+ * @new_node: node to create
+ * @n: integer data for (n) in node
+ * Return: address of new node
+ */
+
+dlistint_t *handle_idx_zero(dlistint_t **h, dlistint_t *new_node, int n)
+{
+	dlistint_t *temp;
+
+	new_node->n = n;
+
+	if (*h == NULL)
+	{
+		new_node->next = *h;
+		*h = new_node;
+		new_node->prev = NULL;
+	}
+
+	else if (*h != NULL)
+	{
+		temp = *h;
+		temp->prev = new_node;
+		new_node->next = *h;
+		*h = new_node;
+		new_node->prev = NULL;
 	}
 
 	return (new_node);
