8d4a78a5c002a10c459c70ae8cacfc6b36433581
bravin-onwonga <onbravin@gmail.com>
2024-02-03 01:44:59 +0300

    Print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the key value pairs in a hash table
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int count = 0;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+			count++;
+	}
+
+	printf("{");
+
+	i = 0;
+	while (i < ht->size && count >= 0)
+	{
+		if (ht->array[i] && count != 0)
+		{
+			printf(" \'%s\' : \'%s\',", ht->array[i]->key, ht->array[i]->value);
+			count--;
+		}
+
+		if (ht->array[i] && count == 0)
+		{
+			printf("\'%s\' : \'%s\' ", ht->array[i]->key, ht->array[i]->value);
+			count--;
+		}
+		i++;
+	}
+	printf("}\n");
+}
