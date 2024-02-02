c1120e06c73cd487aaef510e3accfc80c61218fb
bravin-onwonga <onbravin@gmail.com>
2024-02-03 02:27:50 +0300

    Printing colliding values

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,34 +9,52 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	int count = 0;
+	hash_node_t *temp;
 
-	if (ht)
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
 	{
-
-		for (i = 0; i < ht->size; i++)
-		{
-			if (ht->array[i])
-				count++;
-		}
-
-		printf("{");
-
-		i = 0;
-		while (i < ht->size && count >= 0)
-		{
-			if (ht->array[i] && count != 0)
-			{
-				printf("\'%s\' : \'%s\', ", ht->array[i]->key, ht->array[i]->value);
-				count--;
-			}
-
-			if (ht->array[i] && count == 0)
-			{
-				printf("\'%s\' : \'%s\'", ht->array[i]->key, ht->array[i]->value);
-				count--;
-			}
-			i++;
-		}
-		printf("}\n");
+		if (ht->array[i])
+			count++;
 	}
+
+	printf("{");
+
+	i = 0;
+	while (i < ht->size && count >= 0)
+	{
+		if (ht->array[i] && count != 0)
+		{
+			printf("\'%s\' : \'%s\', ", ht->array[i]->key, ht->array[i]->value);
+
+			temp = ht->array[i]->next;
+			while (temp)
+			{
+				printf("\'%s\' : \'%s\', ", ht->array[i]->key, temp->value);
+				temp = temp->next;
+			}
+			count--;
+		}
+
+		if (ht->array[i] && count == 0)
+		{
+			if (!ht->array[i]->next)
+				printf("\'%s\' : \'%s\'", ht->array[i]->key, ht->array[i]->value);
+			else
+			{
+				printf("\'%s\' : \'%s\', ", ht->array[i]->key, temp->value);
+				temp = ht->array[i]->next;
+				while (temp->next)
+				{
+					printf("\'%s\' : \'%s\', ", ht->array[i]->key, temp->value);
+					temp = temp->next;
+				}
+			}
+			count--;
+		}
+		i++;
+	}
+	printf("}\n");
 }
