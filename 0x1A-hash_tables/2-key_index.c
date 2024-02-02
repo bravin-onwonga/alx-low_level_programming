77dd14c5940e8cb69b51f421290773bcf98b7da0
bravin-onwonga <onbravin@gmail.com>
2024-02-02 23:33:49 +0300

    Get index from key and index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key
+ *
+ * @key: the key of the value
+ * @size: size of the array
+ *
+ * Return: index of key in array
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
